822fef89e49bd1560f8cda4c51c68f48ed503c72
Leon Zagorouiko <Zagorouiko@gmail.com>
2015-08-11 15:55:14 -0700

    add validation for the form.

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -21,7 +21,6 @@ class RafflesController < ApplicationController
   def show
     @raffle = Raffle.find(params[:id])
     @contestants = Contestant.where(raffle_id: @raffle.id)
-    binding.pry
 
   end
 
