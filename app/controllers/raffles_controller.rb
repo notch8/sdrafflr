d7a4b3e6d0a034137fe1685209c1547d0db5eb39
alb <alliebartholomew1@gmail.com>
2015-08-11 10:28:29 -0700

    fixed new raffle form issue, now populates both raffle and contestant tables

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -10,17 +10,15 @@ class RafflesController < ApplicationController
 
   def create
     @raffle = Raffle.new(raffle_params)
-    @contestants = params[:contestants]
-    contestants = @contestants.split("\r\n")
-    binding.pry
-    contestants.each {|contestant| @raffle.contestants << Contestant.new(:name => contestant, :raffle => @raffle)}
-
     if @raffle.save
       render 'show'
     else
       flash[:notice] = "Oops, something went wrong!"
       render 'new'
     end
+    @contestants = params[:contestants]
+    contestants = @contestants.split(/\W+/)
+    contestants.each {|c| @raffle.contestants << Contestant.new(:name => c, :raffle_id => @raffle.id)}
   end
 
   def show
