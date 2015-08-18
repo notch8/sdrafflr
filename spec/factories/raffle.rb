c6713a9c32746b10df0e14de1ec393e28648575c
Matthew Clark <matt@notch8.com>
2015-08-18 09:28:19 -0700

    commit so I can merge

--- spec/factories/raffle.rb
+++ spec/factories/raffle.rb
@@ -4,6 +4,6 @@ require 'faker'
 FactoryGirl.define do
   factory :raffle do |f|
     f.title { Faker::App.name }
-    f.num_winners { Faker::Number.between(1, 3) }
+    f.num_winners { Faker::Number.between(1) }
   end
 end
